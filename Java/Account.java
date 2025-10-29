8c063fe06c7abe7486c4f868cdb9330cf9a911b6
csanadk <49592602+csanadk@users.noreply.github.com>
2025-10-29 02:25:11 +0100

    Create Account.java

---
+++ Java/Account.java
@@ -0,0 +1,33 @@
+package BankDemo;
+
+Public class Account {
+  private double balance;
+
+//Set balance to 0.00
+public Account() {
+  balance = 0.0;
+}
+  //Deposit
+public void deposit(double ammount) {
+  balance = balance + amount;
+}
+  //Withdraw
+public void withdraw(double amount) {
+  if(amount<=balance) {
+    balance = balance - amount;
+  }
+  else {
+    System.err.println("Transaction canceled due to insufficient funds");
+  }
+}
+  //Get Balance
+public double getBalance() {
+  return balance;
+}
+  //public static void main (String[] args) {
+    //TODO Auto-generated method stub
+
+//}
+
+//Closing bracket for line 3:
+}
