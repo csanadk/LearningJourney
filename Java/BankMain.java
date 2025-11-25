09489a7e698cf2875168feac1c3fd4153a2d52b5
Csanad <49592602+csanadk@users.noreply.github.com>
2025-11-25 19:13:57 +0100

    Create BankMain.java
    added java file

---
+++ Java/BankMain.java
@@ -0,0 +1,21 @@
+package BankMain;
+
+public class BankMain {
+
+public static void(String[] args) {
+  //Crate Cust1
+  Account cust1 = new Account(); //Create an account object
+  cust1.deposit(500);
+
+  Create Cust2
+    Account cust2 = new Account();
+  cust2.witdraw(400);
+
+  System.out.print("Cust1 has a balance of ");
+  System.out.println(cust1.getBalanve());
+
+  System.out.print("Cust2 has a balance of ");
+  System.out.println(cust2.getBalance());
+
+}
+}
